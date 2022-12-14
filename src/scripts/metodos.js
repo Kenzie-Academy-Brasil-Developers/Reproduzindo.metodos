4d29274efbd621127994e83fb13cb53696d26d97
diego87 <87.c.die@gmail.com>
2022-12-14 09:48:10 -0300

    commit2

--- src/scripts/metodos.js
+++ src/scripts/metodos.js
@@ -2,74 +2,78 @@
 
 let arr = [0, 1, 2, 3, 4];
 
+
 // ---------------------------------------
 // MÉTODO MAP))
-function callbackmap(value) {
-  return value * 2;
+function callbackmap(currentValue) {
+  return currentValue * 2;
 }
-function newArr(element) {
-  let newArr2 = [];
-  for (let i = 0; i < element.length; i++) {
-    newArr2.push(callbackmap(element[i]));
+function methodmap(arr,callback){
+  let result = [];
+  for (let i = 0; i < arr.length; i++){
+    const value = arr[i]
+    result.push((callback(value, i, arr)));
   }
-  return newArr2;
+  return result;
 }
-console.log(newArr(arr));
+console.log(methodmap(arr,callbackmap))
+
+
 // ---------------------------------------
 // MÉTODO FILTER))
-function callbackfilter(value) {
-  return value;
+function callbackfilter(currentValue) {
+  return currentValue > 2
 }
-function childMore3(idade) {
-  const passed = [];
-  for (let i = 0; i < idade.length; i++) {
-    if (idade[i] > 2) {
-      passed.push(callbackfilter(arr[i]));
-    }
+function methodFilter(arr, callback){
+const result =[]
+for (let i = 0; i < arr.length; i++) {
+  const value = arr[i]
+  if((callback(value, i, arr))){
+    result.push(value)
   }
-  return passed;
 }
-console.log(childMore3(arr));
+  return result
+}
+console.log(methodFilter(arr,callbackfilter))
+
 
 // ---------------------------------------
 // MÉTODO REDUCE))
-function callbackreduce(contador, value) {
-  return (contador += value);
+function callbackreduce(cont, value) {
+  return (cont += value);
 }
-function soma(arr) {
-  let total = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    let result = (total += arr[i]);
-    callbackreduce(result);
+function methodReduce(arr,callback,initialValue){
+  if(initialValue === undefined){
+    initialValue = 0
   }
-  return total;
+  for (let i = 0; i < arr.length; i++) {
+    initialValue = callback(initialValue,arr[i],i,arr)
+  }
+  return initialValue
 }
-console.log(soma(arr));
+console.log(methodReduce(arr,callbackreduce))
 // ----------------------------------------
 // MÉTODO FIND
 function callbackfind(value) {
-  return value;
+  return value > 2
 }
-function comparar(arr) {
-  let num;
-
+function methodFind(arr,callback){
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] == 4) {
-      num = arr[i];
-      callbackfind(num);
-    }
+    const value = arr[i]
+    if((callback(value,i,arr))){
+      return value
+    } 
   }
-  return num;
+  return 'underfined'
 }
-console.log(comparar(arr));
+console.log(methodFind(arr,callbackfind))
 
 // ---------------------------------------
 // MÉTODO INCLUDES
 function callbackincludes(value) {
   return (value = true);
 }
-function metIncludes(arr) {
+function methodIncludes(arr) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === 3) {
       return callbackincludes(arr[i]);
@@ -77,19 +81,34 @@ function metIncludes(arr) {
   }
   return false;
 }
-console.log(metIncludes(arr));
+console.log(methodIncludes(arr,callbackincludes));
+
 // ----------------------------------------
 // MÉTODO INDEXOF
-function callbackIndexOf(value) {
-  return value;
-}
-function metIndexOf(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    let result = arr[i];
-    if (result[i] == 2) {
-      return callbackIndexOf(i);
+function methodIndexOf(arr,searchElement,positionArr){
+  const position = -1
+  if(positionArr === undefined){
+    positionArr = 0
+  }else if (positionArr>=arr.length){
+    return -1
+  }else if (positionArr<0){
+  }
+  if(positionArr>=arr.length){
+    return position
+  }else if (positionArr>=0){
+    for (let i = 0; i < arr.length; i++) {
+      if(arr[i]===searchElement){
+        position = i
+      }
+    }
+  }else if (positionArr<=0){
+    const positionNegative =(arr.length + positionArr)
+    for (let i = positionNegative; i < arr.length; i++) {
+     if(arr[i]===searchElement){
+      position = i
+     } 
     }
   }
-  return -1;
+  return position
 }
-console.log(metIndexOf(arr));
+console.log(methodIndexOf(arr,9,3))
